c0eea61af60cd0d208f9b356730b6eac6ec809f3
ard <aaron@ard.ninja>
2018-03-05 23:09:33 -0600

    add another batch

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 I assembled this collection to NLP on. Free to use, free to learn from. Enjoy! PRs are welcome :)
 
+Not all of these are PDF format, some are saved in text from HTML pages. I try to get PDFs but if I can't find one,
+I just try to save it as a .txt file with only text and no markup.
+
+If you make a PR, please make sure to save the file as either PDF or text and make an entry into the __papers-index.csv__ file!
 
 
 ### Development
 
-I wrote some node scripts to spider sites and download PDFs.
+I wrote a script to make entries to the index and download PDFs. It is still a very manual process.
 
 To run locally install node 8 + npm and run npm install first.
--- downloader.js
+++ downloader.js
@@ -6,6 +6,9 @@ const async = require('async')
 
 prompt.start();
 
+// sad that cryptocurrency whitepapers are served under invalid certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 async.whilst(() => 1, (callback) => {
   prompt.get(['name', 'code', 'url'], function (err, result) {
     console.log('Command-line input received:', result);
@@ -27,23 +30,3 @@ async.whilst(() => 1, (callback) => {
 function appendCsv(name, code, link, pdf) {
   fs.appendFileSync('./papers-index.csv', [name,code,link,pdf].join(',') + '\n')
 }
-
-
-// appendCsv(result.name, result.code, result.link, 'whitepapers/pdf.pdf')
-// const options = {
-//   method: 'GET',
-//   url: result.link,
-//   headers: {
-//     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36',
-//     'Cache-Control': 'max-age=0'
-//   }
-// };
-// request(options, function(err, res, body) {
-//   if (error || res.statusCode !== 200 || !body) {
-//     console.error(res.statusCode)
-//     console.error(body)
-//     process.exit()
-//   }
-//
-//   const filename = `whitepapers/${result.name}-${result.code}-whitepaper`
-// })
